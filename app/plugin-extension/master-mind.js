6474d414dc51a00ed869ba06571391b35bf57a86
jmarti <jmarti@theinit.com>
2018-12-02 15:57:40 +0100

    improve numeralizer in master mind

--- app/plugin-extension/master-mind.js
+++ app/plugin-extension/master-mind.js
@@ -1,15 +1,15 @@
 const numeralize = x =>
-  x.replace(/ /g, '')
-    .replace(/uno/g, '1')
-    .replace(/dos/g, '2')
-    .replace(/tres/g, '3')
-    .replace(/cuatro/g, '4')
-    .replace(/cinco/g, '5')
-    .replace(/seis/g, '6')
-    .replace(/siete/g, '7')
-    .replace(/ocho/g, '8')
-    .replace(/nueve/g, '9')
-    .replace(/cero/g, '0');
+  x.replace(/\buno\b/g, '1')
+    .replace(/\bdos\b/g, '2')
+    .replace(/\btres\b/g, '3')
+    .replace(/\bcuatro\b/g, '4')
+    .replace(/\bcinco\b/g, '5')
+    .replace(/\bseis\b/g, '6')
+    .replace(/\bsiete\b/g, '7')
+    .replace(/\bocho\b/g, '8')
+    .replace(/\bnueve\b/g, '9')
+    .replace(/\bcero\b/g, '0')
+    .replace(/\D/g, '');
 
 const shuffle = array => array
   .map((a) => ({sort: Math.random(), value: a}))
